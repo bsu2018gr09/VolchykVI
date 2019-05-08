ecba5bdd901744515719531bdd2520ea7fb2a60d
Vladislav <volf10011@gmail.com>
2019-05-08 14:06:25 +0300

    Improved optimization

--- 1-1.cpp
+++ 1-1.cpp
@@ -10,60 +10,59 @@
 
 using namespace std;
 
+typedef struct point {
+	int X;
+	int Y;
+	double S;
+};
+
 template <class T>
 T* constructor(int);
 int getSize();
 template <class T>
 void destructor(T*);
 int getSize();
-void fillWithRand(int*, int, int);
+void fillWithRand(point*, int, int);
 int getRange();
-void findDistance(double*, int*, int*, int, double, double, double);
+void findDistance(point*, int, double, double, double);
 void getCoffs(double&, double&, double&);
-void sortPoints(int*, int*, double*, int);
-void outputPoints(int*, int*, double*, int);
+void sortPoints(point*, int);
+void outputPoints(point*, int);
+
+
 
 int main() {
 
 	setlocale(LC_ALL, "rus");
 	srand(time(0));
 	int N = getSize();
-	int* X = constructor<int>(N);
-	int* Y = constructor<int>(N);
-	double* S = constructor<double>(N);
+	point* V = constructor<point>(N);
 	int range = getRange();
-	fillWithRand(X, N, range);
-	fillWithRand(Y, N, range);
+	fillWithRand(V, N, range);
 	double a, b, c;
 	getCoffs(a, b, c);
-	findDistance(S, X, Y, N, a, b, c);
-	outputPoints(X, Y, S, N);
-	sortPoints(X, Y, S, N);
-	outputPoints(X, Y, S, N);
-	destructor(X);
-	destructor(Y);
-	destructor(S);
+	findDistance(V, N, a, b, c);
+	outputPoints(V, N);
+	sortPoints(V, N);
+	outputPoints(V, N);
+	destructor<point>(V);
 
 	system("pause");
 	return 0;
 }
 
-void outputPoints(int* X, int* Y, double* S, int N) {
+void outputPoints(point* V, int N) {
 	cout << "  X  Y " << "Метрика\n==============\n";
 	for (int i = 0; i < N; i++) {
-		cout << setw(3) << *(X + i) << setw(3) << *(Y + i) << ' ' << *(S + i) << endl;
+		cout << setw(3) << (V + i)->X << setw(3) << (V + i)->Y << ' ' << (V + i)->S << endl;
 	}
 	cout << endl;
 }
 
-void sortPoints(int *X, int *Y, double *S, int N) {// совсем никакой оптимизации (((
+void sortPoints(point* V, int N) {
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N - 1 - i; j++) {
-			if (*(S + j) > *(S + j + 1)) {
-				swap(*(S + j), *(S + j + 1));
-				swap(*(X + j), *(X + j + 1));
-				swap(*(Y + j), *(Y + j + 1));
-			}
+			if ((V + j)->S > (V + j + 1)->S) { swap(*(V + j), *(V + j + 1)); }
 		}
 	}
 }
@@ -73,9 +72,10 @@ void getCoffs(double& a, double& b, double& c) {
 	cin >> a >> b >> c;
 }
 
-void findDistance(double* S, int* X, int* Y, int N, double a, double b, double c) {
-	while (N-- > 0) {
-		*(S++) = (abs(*(X++)*a + *(Y++)*b + c) / sqrt((a*a) + (b*b)));// совсем никакой оптимизации. Позор ((( 
+void findDistance(point* V, int N, double a, double b, double c) {
+	double l = sqrt((a*a) + (b*b));
+	for (int i = 0; i < N; i++) {
+		(V + i)->S = abs((V + i)->X*a + (V + i)->Y*b + c) / l;
 	}
 }
 
@@ -86,9 +86,11 @@ int getRange() {
 	return range;
 }
 
-void fillWithRand(int* A, int N, int range) {
-	while (N-- > 0)
-		*(A++) = rand() % range;
+void fillWithRand(point* V, int N, int range) {
+	for (int i = 0; i < N; i++) {
+		(V + i)->X = rand() % range;
+		(V + i)->Y = rand() % range;
+	}
 }
 
 int getSize() {
